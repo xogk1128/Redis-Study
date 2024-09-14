afa3472bb963e31753b6fdf172dd15dad358bf06
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-09-14 17:35:54 +0900

    🚩 Redis 세팅 추가

--- redis-in-spring/src/main/java/com/example/redisinspring/BoardService.java
+++ redis-in-spring/src/main/java/com/example/redisinspring/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.redisinspring;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,7 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size){
         Pageable pageable = PageRequest.of(page-1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
